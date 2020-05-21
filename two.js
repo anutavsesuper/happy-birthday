1b723614467ed85715221e6235cb0008da206689
anutavsesuper <55274885+anutavsesuper@users.noreply.github.com>
2020-05-22 01:15:28 +0300

    Add files via upload

---
+++ two.js
@@ -0,0 +1,18 @@
+let love = 100;
+
+function setup() {
+ createCanvas(windowWidth, windowHeight);
+  background(0);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  noFill();
+  stroke(250, sin(love)*250, 100);
+  strokeWeight(5);
+  ellipse(mouseX, windowHeight-mouseY, 60, 60);
+  love += 0.08;
+}
