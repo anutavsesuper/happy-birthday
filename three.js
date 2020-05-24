6dc4200380a112b2d962bbfa4bbee19c588c78b4
anutavsesuper <55274885+anutavsesuper@users.noreply.github.com>
2020-05-24 20:57:51 +0300

    Add files via upload

--- three.js
+++ three.js
@@ -1,47 +1,29 @@
-let symmetry = 6;
-let sw = 0;
-
-let angle = 360 / symmetry;
+let love;
+let l;
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // background('#FA6964');
-  // background('#FA6964');
-  background(0);
-
-  angleMode(DEGREES);
+  love = createCanvas(windowWidth, windowHeight);
+  love.mouseOver(changeD);
+  l = 20;
 }
 
-
 function draw() {
-  translate(width*0.48, height*0.5);
+  background(105, 164, 255);
+  noStroke()
 
-  if (mouseX > 0
-      && mouseX < width
-      && mouseY > 0
-      && mouseY < height) {
-    let mx = mouseX - width / 2;
-    let my = mouseY - height / 2;
-    let pmx = pmouseX - width / 2;
-    let pmy = pmouseY - height / 2;
+  ellipse(width / 2, height / 2, l, l);
 
-    if (mouseIsPressed) {
-      for (let i = 0; i < symmetry; i++) {
-        rotate(angle);
-        stroke(250, sin(sw)*200, 100)
-        // sin(sw)*
-        strokeWeight(3);
-        line(mx, my, pmx, pmy);
-        push();
-        scale(1, -1);
-        line(mx, my, pmx, pmy);
-        pop();
-        sw += 0.7;
-      }
-    }
+    if (mouseIsPressed == true) {
+    fill(250, 105, 100);
   }
+  else {
+    fill(250, 205, 100);
+  }
+}
+function changeD() {
+  l = l + 40;
 }
--- zero.js
+++ zero.js
@@ -1,85 +1,50 @@
-function setup() {
- createCanvas(windowWidth, windowHeight);
-  background(0);
-}
+let bugs = [];
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < 100; i++) {
+    bugs.push(new Love());
+  }
 }
 
 function draw() {
-  background(0);
-  noStroke()
-  
-  push();
-  translate(width * 0.9, height * 0.2);
-  rotate(frameCount / -100.0);
-  star(0, 0, 20, 30, 5);
-  pop();
-  
-  push();
-  translate(width * 0.15, height * 0.85);
-  rotate(frameCount / 100.0);
-  star(0, 0, 30, 45, 5);
-  pop();
-  
-  push();
-  translate(width * 0.4, height * 0.1);
-  rotate(frameCount / 100.0);
-  star(0, 0, 5, 7, 5);
-  pop();
-  
-  push();
-  translate(width * 0.8, height * 0.5);
-  rotate(frameCount / 100.0);
-  star(0, 0, 7, 11, 5);
-  pop();
-  
-  push();
-  translate(width * 0.5, height * 0.92);
-  rotate(frameCount / -100.0);
-  star(0, 0, 10, 15, 5);
-  pop();
-  
-  push();
-  translate(width * 0.1, height * 0.5);
-  rotate(frameCount / -100.0);
-  star(0, 0, 10, 15, 5);
-  pop();
-  
-  push();
-  translate(width * 0.8, height * 0.8);
-  rotate(frameCount / 100.0);
-  star(0, 0, 15, 23, 5);
-  pop();
-  
-  push();
-  translate(width * 0.2, height * 0.2);
-  rotate(frameCount / 100.0);
-  star(0, 0, 15, 23, 5);
-  pop();
-  
-  push();
-  translate(width * 0.7, height * 0.2);
-  rotate(frameCount / 100.0);
-  star(0, 0, 10, 15, 5);
-  pop();
-  
-  
-  
-  if (mouseIsPressed == true) {
-    fill(250, 105, 100); // White
-  }
-  else {
-    fill(105, 164, 255);   // Black
+  background('#000');
+  // stroke('#FFFFFF');
+  // strokeWeight(1);
+  noStroke();
+  fill('#69A4FF');
+  for (let i = 0; i < bugs.length; i++) {
+    bugs[i].move();
+    bugs[i].display();
   }
 }
+
+// Love class
+class Love {
+  constructor() {
+    this.x = random(width);
+    this.y = random(height);
+    this.diameter = random(7, 25);
+    this.speed = 1.2;
+  }
+
+  move() {
+    this.x += random(-this.speed, this.speed);
+    this.y += random(-this.speed, this.speed);
+  }
+
+  display() {
+    // ellipse(this.x, this.y, this.diameter, this.diameter);
+    star(this.x, this.y, this.diameter, this.diameter * 0.6, 5)
+}
+}
+
 
 function star(x, y, radius1, radius2, npoints) {
   let angle = TWO_PI / npoints;
   let halfAngle = angle / 2.0;
   beginShape();
-  for (let a = 0; a < TWO_PI; a += angle) {
+  for (let a = -0.95; a < TWO_PI; a += angle) {
     let sx = x + cos(a) * radius2;
     let sy = y + sin(a) * radius2;
     vertex(sx, sy);
@@ -89,3 +54,98 @@ function star(x, y, radius1, radius2, npoints) {
   }
   endShape(CLOSE);
 }
+
+
+// one {
+// function setup() {
+//  createCanvas(windowWidth, windowHeight);
+//   background(0);
+// }
+//
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight);
+// }
+//
+// function draw() {
+//   background(0);
+//   noStroke()
+//
+//   push();
+//   translate(width * 0.9, height * 0.2);
+//   rotate(frameCount / -100.0);
+//   star(0, 0, 20, 30, 5);
+//   pop();
+//
+//   push();
+//   translate(width * 0.15, height * 0.85);
+//   rotate(frameCount / 100.0);
+//   star(0, 0, 30, 45, 5);
+//   pop();
+//
+//   push();
+//   translate(width * 0.4, height * 0.1);
+//   rotate(frameCount / 100.0);
+//   star(0, 0, 5, 7, 5);
+//   pop();
+//
+//   push();
+//   translate(width * 0.8, height * 0.5);
+//   rotate(frameCount / 100.0);
+//   star(0, 0, 7, 11, 5);
+//   pop();
+//
+//   push();
+//   translate(width * 0.5, height * 0.92);
+//   rotate(frameCount / -100.0);
+//   star(0, 0, 10, 15, 5);
+//   pop();
+//
+//   push();
+//   translate(width * 0.1, height * 0.5);
+//   rotate(frameCount / -100.0);
+//   star(0, 0, 10, 15, 5);
+//   pop();
+//
+//   push();
+//   translate(width * 0.8, height * 0.8);
+//   rotate(frameCount / 100.0);
+//   star(0, 0, 15, 23, 5);
+//   pop();
+//
+//   push();
+//   translate(width * 0.2, height * 0.2);
+//   rotate(frameCount / 100.0);
+//   star(0, 0, 15, 23, 5);
+//   pop();
+//
+//   push();
+//   translate(width * 0.7, height * 0.2);
+//   rotate(frameCount / 100.0);
+//   star(0, 0, 10, 15, 5);
+//   pop();
+//
+//
+//
+//   if (mouseIsPressed == true) {
+//     fill(250, 105, 100);
+//   }
+//   else {
+//     fill(105, 164, 255);
+//   }
+// }
+//
+// function star(x, y, radius1, radius2, npoints) {
+//   let angle = TWO_PI / npoints;
+//   let halfAngle = angle / 2.0;
+//   beginShape();
+//   for (let a = 0; a < TWO_PI; a += angle) {
+//     let sx = x + cos(a) * radius2;
+//     let sy = y + sin(a) * radius2;
+//     vertex(sx, sy);
+//     sx = x + cos(a + halfAngle) * radius1;
+//     sy = y + sin(a + halfAngle) * radius1;
+//     vertex(sx, sy);
+//   }
+//   endShape(CLOSE);
+// }
+// }
